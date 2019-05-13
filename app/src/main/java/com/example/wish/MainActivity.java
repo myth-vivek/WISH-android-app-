6703b96fe406df8e12b0251aca4007b2b96c5a23
vivek Magar <vivekmgr229@gmail.com>
2019-05-14 00:36:38 +0545

    splash screen generated

--- app/src/main/java/com/example/wish/MainActivity.java
+++ app/src/main/java/com/example/wish/MainActivity.java
@@ -7,6 +7,7 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        setTheme(R.style.splashScreenTheme);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
